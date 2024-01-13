3be602ef74e41cb5e77665082c1044c4934c222f
Matthew Neff <mpneff19@gmail.com>
2024-01-13 13:09:29 -0500

    complete Hello World

---
+++ Hello World/hello_world.py
@@ -0,0 +1,9 @@
+# Prints "Hello World" as a beginning project
+# Created by Matthew Neff
+
+def main():
+    '''Prints Hello World'''
+    print('Hello World!')
+
+if __name__ == '__main__':
+    main()
