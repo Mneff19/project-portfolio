910b6d50e11e9f7434772d41bf75d7ecd3f86d29
Matthew Neff <mpneff19@gmail.com>
2024-04-02 08:34:21 -0600

    adjusted urls for app

--- EventBooking/EventBooking/urls.py
+++ EventBooking/EventBooking/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("calendar/", include("EventCalendar.urls")),
+    path("", include("EventCalendar.urls")),
     path("admin/", admin.site.urls),
 ]
